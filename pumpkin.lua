fb63dd4ae208c95103911c80bb635e2b576e04df
Juraj Vajda <juraj.vajda@rituals.com>
2017-12-12 17:37:47 -0500

    added pumpkin

---
+++ pumpkin.lua
@@ -0,0 +1,213 @@
+local minlight = 13
+local maxlight = default.LIGHT_MAX
+
+local function pumpkin_on_construct(pos)
+	for i = 1,2 do
+		if minetest.get_node({x=pos.x,y=pos.y-i,z=pos.z}).name ~= "default:snowblock" then
+			return
+		end
+	end
+	
+	--if 3 snow block are placed, this will make snowman
+	
+	for i = 0,2 do
+		minetest.remove_node({x=pos.x,y=pos.y-i,z=pos.z})
+	end
+
+	minetest.add_entity({x=pos.x,y=pos.y-1,z=pos.z}, "farming_addons:snow_golem")
+end
+
+-- PUMPKIN
+farming.register_plant("farming_addons:pumpkin", {
+	description = "Pumpkin Seed",
+	inventory_image = "farming_addons_pumpkin_seed.png",
+	steps = 8,
+	minlight = minlight,
+	maxlight = maxlight,
+	fertility = {"grassland"},
+	groups = {flammable = 4},
+	place_param2 = 3,
+})
+
+-- PUMPKIN FRUIT - HARVEST
+minetest.register_node("farming_addons:pumpkin_fruit", {
+	description = "Pumpkin Fruit",
+	tiles = {"farming_addons_pumpkin_fruit_top.png", "farming_addons_pumpkin_fruit_top.png", "farming_addons_pumpkin_fruit_side.png", "farming_addons_pumpkin_fruit_side.png", "farming_addons_pumpkin_fruit_side.png", "farming_addons_pumpkin_fruit_side_off.png"},
+	paramtype2 = "facedir",
+	sounds = default.node_sound_wood_defaults(),
+	is_ground_content = false,
+	groups = {snappy=3, flammable=4, fall_damage_add_percent=-30},
+		drop = {
+		max_items = 4,  -- Maximum number of items to drop.
+		items = { -- Choose max_items randomly from this list.
+			{
+				items = {"farming_addons:pumpkin"},  -- Items to drop.
+				rarity = 1,  -- Probability of dropping is 1 / rarity.
+			},
+			{
+				items = {"farming_addons:pumpkin"},  -- Items to drop.
+				rarity = 2,  -- Probability of dropping is 1 / rarity.
+			}
+		},
+	},
+})
+
+-- PUMPKIN BLOCK - HARVEST from crops
+minetest.register_node("farming_addons:pumpkin_block", {
+	description = "Pumpkin Fruit",
+	tiles = {"farming_addons_pumpkin_fruit_top.png", "farming_addons_pumpkin_fruit_top.png", "farming_addons_pumpkin_fruit_side.png", "farming_addons_pumpkin_fruit_side.png", "farming_addons_pumpkin_fruit_side.png", "farming_addons_pumpkin_fruit_side_off.png"},
+	paramtype2 = "facedir",
+	sounds = default.node_sound_wood_defaults(),
+	is_ground_content = false,
+	groups = {snappy=3, flammable=4, fall_damage_add_percent=-30},
+	on_construct = pumpkin_on_construct
+})
+
+-- PUMPKIN LANTERN -- from recipe
+minetest.register_node("farming_addons:pumpkin_lantern", {
+	description = "Pumpkin Lantern",
+	tiles = {"farming_addons_pumpkin_fruit_top.png", "farming_addons_pumpkin_fruit_top.png", "farming_addons_pumpkin_fruit_side.png", "farming_addons_pumpkin_fruit_side.png", "farming_addons_pumpkin_fruit_side.png", "farming_addons_pumpkin_fruit_side_on.png"},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	sounds = default.node_sound_wood_defaults(),
+	is_ground_content = false,
+	light_source = 12,
+	drop = "farming_addons:pumpkin_lantern",
+	groups = {snappy=3, flammable=4, fall_damage_add_percent=-30},
+	on_construct = pumpkin_on_construct
+})
+
+-- pumpkin lantern recipe
+minetest.register_craft({
+	output = "farming_addons:pumpkin_lantern",
+	recipe = {
+		{"", "", ""},
+		{"", "farming_addons:pumpkin_block", ""},
+		{"", "default:torch", ""}
+	},
+})
+
+-- crafted item from recipe
+minetest.register_craftitem("farming_addons:pumpkin_pie", {
+	description = "Pumpkin Pie",
+	inventory_image = "farming_addons_pumpkin_pie.png",
+	groups = {fleshy = 3, dig_immediate = 3, flammable = 2},
+	on_use = minetest.item_eat(6),
+	sounds = default.node_sound_leaves_defaults()
+})
+
+-- pumpkin pie crafting recipe
+minetest.register_craft({
+	output = "farming_addons:pumpkin_pie",
+	recipe = {
+		{"", "", ""},
+		{"farming_addons:pumpkin_block", "farming:flour", ""},
+		{"", "mobs:egg", ""}
+	}
+})
+
+-- needed for hbhunger
+minetest.override_item("farming_addons:pumpkin_pie", {
+	on_use = minetest.item_eat(6),
+})
+
+-- drop blocks instead of items
+minetest.register_alias_force("farming_addons:pumpkin", "farming_addons:pumpkin_block")
+
+-- drops pumpkin seeds
+-- minetest.override_item("default:dry_shrub", {
+-- 	drop = {
+-- 		max_items = 1,
+-- 		items = {
+-- 			{items = {'farming_addons:seed_pumpkin'}, rarity = 5},
+-- 			{items = {'default:dry_shrub'}},
+-- 		}
+-- 	}
+-- })
+
+for i = 1, 5 do
+	minetest.override_item("default:dry_grass_"..i, {drop = {
+		max_items = 1,
+		items = {
+			{items = {"farming_addons:seed_pumpkin"}, rarity = 6},
+			{items = {"default:dry_grass_1"}},
+		}
+	}})
+end
+
+local function grow_pumpkin(pos, elapsed)
+	local node = minetest.get_node(pos)
+	local random_pos = false
+	local spawn_positions = {}
+	local right_pos = {x=pos.x+1, y=pos.y, z=pos.z}
+	local front_pos = {x=pos.x, y=pos.y, z=pos.z+1}
+	local left_pos = {x=pos.x-1, y=pos.y, z=pos.z}
+	local back_pos = {x=pos.x, y=pos.y, z=pos.z-1}
+	local right = minetest.get_node(right_pos)
+	local front = minetest.get_node(front_pos)
+	local left = minetest.get_node(left_pos)
+	local back = minetest.get_node(back_pos)
+
+	if right.name == "farming_addons:pumpkin_fruit"
+	or front.name == "farming_addons:pumpkin_fruit"
+	or left.name == "farming_addons:pumpkin_fruit"
+	or back.name == "farming_addons:pumpkin_fruit" then
+		return
+	end
+
+	-- make sure that at least one side of the plant has space to put pumpkin
+	if right.name == "air" then
+		table.insert(spawn_positions, right_pos)
+	end
+	if front.name == "air" then
+		table.insert(spawn_positions, front_pos)
+	end
+	if left.name == "air" then
+		table.insert(spawn_positions, left_pos)
+	end
+	if back.name == "air" then
+		table.insert(spawn_positions, back_pos)
+	end
+
+	if #spawn_positions < 1 then
+		-- plant is closed from all sides
+		return false
+	else
+		-- pick random from the open sides
+		local pick_random
+
+		if #spawn_positions == 1 then
+			pick_random = #spawn_positions
+		else
+			pick_random = math.random(1,#spawn_positions)
+		end
+		
+		for k, v in pairs (spawn_positions) do
+			if k == pick_random then
+				random_pos = v
+			end
+		end
+	end
+
+	-- check light
+	local light = minetest.get_node_light(pos)
+	if not light or light < minlight or light > maxlight then
+		return
+	end
+
+	if random_pos then
+		-- spawn pumpkin
+		minetest.set_node(random_pos, {name="farming_addons:pumpkin_fruit"})
+	end
+
+end
+
+minetest.register_abm({
+	label = "pumpkin_growing_abm",
+	nodenames = {"farming_addons:pumpkin_8"},
+	neighbors = {"air"},
+	interval = 900,
+	chance = 2,
+	catch_up = true,
+	action = grow_pumpkin
+})
---
+++ snow_golem.lua
@@ -0,0 +1,196 @@
+local S = mobs.intllib
+
+mobs.npc_drops = {
+  "default:mese_crystal",
+  "default:pick_steel",
+  "default:pick_bronze",
+  "default:sword_steel",
+  "default:sword_bronze",
+  "default:shovel_steel",
+  "default:shovel_bronze",
+  "default:axe_steel",
+  "default:axe_bronze",
+  "farming:bread",
+  "farming:hoe_steel",
+  "farming:hoe_bronze",
+  "bucket:bucket_water",
+  "mobs:meat",
+  "mobs:beehive",
+  "mobs:egg",
+  "mobs:chicken_cooked",
+  "mobs:bucket_milk",
+  "mobs:cheese",
+  "mobs:pork_cooked",
+  "farming_addons:pumpkin_block",
+  "farming_addons:melon_fruit",
+  "screwdriver:screwdriver",
+  "default:diamond",
+  "default:gold_lump",
+  "default:coalblock"
+}
+
+local snow_golem_def = {
+  type = "npc",
+  passive = false,
+  pathfinding = true,
+  docile_by_day = true,
+  attack_type = "dogshoot",
+  attacks_monsters = true,
+  damage = 5,
+  group_attack = true,
+  owner_loyal = true,
+  shoot_interval = 0.9,
+  dogshoot_switch = 1,
+  dogshoot_count_max = 10,
+  arrow = "farming_addons:snowball_arrow",
+  shoot_offset = 1,
+  reach = 2,
+  damage = 3,
+  hp_min = 35,
+  hp_max = 70,
+  armor = 100,
+  collisionbox = {-0.35, -0.1, -0.35, 0.35, 1.89, 0.35},
+  visual_size = {x = 2.9, y = 2.9},
+  visual = "mesh",
+  mesh = "farming_addons_snowman.b3d",
+  drawtype = "front",
+  textures = {
+    {"farming_addons_snowman.png^farming_addons_snowman_pumpkin.png"},
+  },
+  blood_texture = "default_snowball.png",
+  owner = "",
+  order = "follow",
+  makes_footstep_sound = true,
+  walk_velocity = 0.6,
+  run_velocity = 2,
+  view_range = 15,
+  jump = true,
+  floats = 1,
+  drops = {
+    {name = "default:snow", chance = 1, min = 0, max = 15},
+    {name = "farming_addons:seed_pumpkin", chance = 5, min = 1, max = 3}
+  },
+  follow = {"farming:bread", "mobs:meat", "default:diamond"},
+  water_damage = 6,
+  lava_damage = 10,
+  light_damage = 0,
+  fear_height = 4,
+  animation = {
+    speed_normal = 25,
+    speed_run = 50,
+    stand_start = 20,
+    stand_end = 40,
+    walk_start = 0,
+    walk_end = 20,
+    run_start = 0,
+    run_end = 20,
+    die_start = 40,
+    die_end = 50,
+    die_loop = false,
+  },
+  on_rightclick = function(self, clicker)
+
+      -- feed to heal npc
+      if mobs:feed_tame(self, clicker, 8, true, true) then return end
+
+      -- capture npc with net or lasso
+      if mobs:capture_mob(self, clicker, 0, 5, 80, false, nil) then return end
+
+      -- protect npc with mobs:protector
+      if mobs:protect(self, clicker) then return end
+
+      local item = clicker:get_wielded_item()
+      local name = clicker:get_player_name()
+
+      -- right clicking with gold lump drops random item from mobs.npc_drops
+      if item:get_name() == "default:gold_lump" then
+
+        if not mobs.is_creative(name) then
+          item:take_item()
+          clicker:set_wielded_item(item)
+        end
+
+        local pos = self.object:get_pos()
+
+        pos.y = pos.y + 0.5
+
+        -- add item if it exists
+        local obj = minetest.add_item(pos, {
+          name = mobs.npc_drops[math.random(1, #mobs.npc_drops)]
+        })
+
+        if obj and obj:get_luaentity() then
+          obj:setvelocity({
+            x = math.random(-10, 10) / 9,
+            y = 6,
+            z = math.random(-10, 10) / 9,
+          })
+        elseif obj then
+          obj:remove() -- item does not exist
+        end
+
+        minetest.chat_send_player(name, S("Snow Golem dropped you an item for gold!"))
+
+        return
+      end
+
+      -- by right-clicking owner can switch npc between follow and stand
+      if self.owner and self.owner == name then
+
+        if self.order == "follow" then
+          self.order = "stand"
+
+          minetest.chat_send_player(name, S("NPC stands still."))
+        else
+          self.order = "follow"
+
+          minetest.chat_send_player(name, S("NPC will follow you."))
+        end
+      end
+
+    end,
+}
+
+mobs:register_mob("farming_addons:snow_golem", snow_golem_def)
+
+-- mobs:spawn({
+--   name = "farming_addons:snow_golem",
+--   nodes = {"default:desert_sand", "default:desert_stone", "default:sand", "default:sandstone", "default:silver_sand", "farming_addons:deco_stone_eye", "farming_addons:deco_stone_men", "farming_addons:deco_stone_sun"},
+--   min_light = 0,
+--   max_light = 20,
+--   chance = 2000,
+--   active_object_count = 2,
+--   day_toggle = false,
+-- })
+
+mobs:register_egg("farming_addons:snow_golem", "Snow Golem", "default_snow.png", 1)
+
+-- shooting
+mobs:register_arrow("farming_addons:snowball_arrow", {
+  visual = "sprite",
+  visual_size = {x = 1, y = 1},
+  textures = {"default_snowball.png"},
+  velocity = 14,
+  tail = 0,
+  tail_texture = "default_snowball.png",
+  glow = 5,
+  -- tail_size = 10,
+
+  -- direct hit, no fire... just plenty of pain
+  hit_player = function(self, player)
+    player:punch(self.object, 1.0, {
+      full_punch_interval = 1.0,
+      damage_groups = {fleshy = 8},
+    }, nil)
+  end,
+
+  hit_mob = function(self, player)
+    player:punch(self.object, 1.0, {
+      full_punch_interval = 1.0,
+      damage_groups = {fleshy = 8},
+    }, nil)
+  end,
+
+  hit_node = function(self, pos, node)
+  end
+})

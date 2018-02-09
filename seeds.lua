4e3a74d1da505b7cc39669390013c5dea50d6d1b
Juraj Vajda <juraj.vajda@gmail.com>
2018-02-08 19:38:37 -0500

    add seed overrides to minetest decorations - plants, grass, bush stem

--- seeds.lua
+++ seeds.lua
@@ -12,11 +12,6 @@ minetest.override_item("flowers:mushroom_red", {drop = {
 }})
 
 -- beetroot
--- carrot
--- coffee
--- corn
--- melon
--- potato
 -- pumpkin
 for i = 1, 5 do
   minetest.override_item("default:dry_grass_"..i, {drop = {
@@ -29,20 +24,46 @@ for i = 1, 5 do
   }})
 end
 
+-- carrot
+-- wheat
 for i = 1, 5 do
   minetest.override_item("default:grass_"..i, {drop = {
     max_items = 1,
     items = {
-      {items = {'farming:seed_wheat'},rarity = 5},
-      {items = {'default:grass_1'}},
+      {items = {"farming:seed_wheat"},rarity = 5},
+      {items = {"farming_addons:seed_carrot"}, rarity = 6},
+      {items = {"default:grass_1"}},
     }
   }})
 end
 
+-- potato
+-- cotton
 minetest.override_item("default:junglegrass", {drop = {
   max_items = 1,
   items = {
-    {items = {'farming:seed_cotton'},rarity = 8},
-    {items = {'default:junglegrass'}},
+    {items = {"farming:seed_cotton"},rarity = 8},
+    {items = {"farming_addons:seed_potato"}, rarity = 6},
+    {items = {"default:junglegrass"}},
+  }
+}})
+
+-- coffee
+minetest.override_item("default:bush_stem", {drop = {
+  max_items = 1,
+  items = {
+    {items = {"farming_addons:seed_coffee"}, rarity = 6},
+    {items = {"default:bush_stem"}},
+  }
+}})
+
+-- corn
+-- melon
+minetest.override_item("default:dry_shrub", {drop = {
+  max_items = 1,
+  items = {
+    {items = {"farming_addons:seed_corn"},rarity = 6},
+    {items = {"farming_addons:seed_melon"}, rarity = 6},
+    {items = {"default:dry_shrub"}},
   }
 }})

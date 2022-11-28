ad35ff45da8f63abe0a8afa6e537dfba4b9d703a
Juraj Vajda <juraj.vajda@gmail.com>
2022-11-28 15:52:17 -0500

    Update licensing

--- bonemeal.lua
+++ bonemeal.lua
@@ -1,8 +1,20 @@
-------------
---Bonemeal Mod for Minetest by SaKeL
----@author Juraj Vajda
----@license GNU LGPL 2.1 or later
-----
+--[[
+    X Farming Bonemeal. Extends Minetest farming mod with new plants, crops and ice fishing.
+    Copyright (C) 2022 SaKeL <juraj.vajda@gmail.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to juraj.vajda@gmail.com
+--]]
 
 x_default = minetest.global_exists('x_default') and x_default --[[@as table]]
 
--- salt.lua
+++ salt.lua
@@ -1,3 +1,21 @@
+--[[
+    X Farming. Extends Minetest farming mod with new plants, crops and ice fishing.
+    Copyright (C) 2022 SaKeL <juraj.vajda@gmail.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to juraj.vajda@gmail.com
+--]]
+
 screwdriver = screwdriver --[[@as MtgScrewdriver]]
 
 local S = minetest.get_translator(minetest.get_current_modname())
